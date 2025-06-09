f357d27903ac1b73f907a758b92568ebc5b4c83a
Monika <monikabaghumyan0@gmail.com>
2025-06-09 20:50:44 +0400

    Initial commit

---
+++ task-manager/src/App.jsx
@@ -0,0 +1,12 @@
+import AddTransaction from "./AddTransaction"; 
+import "./App.css"    
+
+export default function App () {
+    return ( 
+        <>  
+        <AddTransaction />  
+         
+
+          </>
+    )
+}
